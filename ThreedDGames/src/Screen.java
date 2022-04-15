1111b0687e7344e4e6d8d700152d588ed7daa29a
velovan <velovan@users.noreply.github.com>
2022-04-15 16:05:38 +0300

    Update Screen.java

--- ThreedDGames/src/Screen.java
+++ ThreedDGames/src/Screen.java
@@ -7,18 +7,15 @@ public class Screen extends BitMap{
 	public BitMap test;
 	public BitMap3D perspectiveVision;
 	
-	public Screen(int width, int height) {
-		
-		super(width, height);
-		
+	public Screen(int width, int height){
+		super(width, height);	
 		test = new BitMap(50, 50);
 		for(int i = 0; i < test.pixels.length; i++)
-			test.pixels[i] = r.nextInt();
-			perspectiveVision = new BitMap3D(width, height);
+		  test.pixels[i] = r.nextInt();
+		  perspectiveVision = new BitMap3D(width, height);
 	}
 	
-	public void render(Game game){
-		
+	public void render(Game game){		
 		clear();
 		perspectiveVision.render(game);
 		perspectiveVision.renderFog();
@@ -27,7 +24,7 @@ public class Screen extends BitMap{
 	//  render(test,(width - 50) / 2 + ox ,(height - 50) / 2 + oy );
 	}
 	
-	public void update(){
-		 
+	public void update(){		 
 	}
+	
 }
