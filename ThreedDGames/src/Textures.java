b7678faf516b6edb5e5e7695e78f83c38d9cce77
velovan <velovan@users.noreply.github.com>
2022-04-17 14:17:41 +0300

    Update Textures.java

--- ThreedDGames/src/Textures.java
+++ ThreedDGames/src/Textures.java
@@ -4,26 +4,24 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Textures {
-
 	
 	public static BitMap floor = loadTexture("/textures/floors.png");
-	public static BitMap loadTexture(String path){
-		
-		
+	
+	public static BitMap loadTexture(String path){	
 		try {
 		    BufferedImage image = ImageIO.read(Textures.class.getResourceAsStream(path));
 			BitMap res = new BitMap(image.getWidth(),image.getHeight());
 			image.getRGB(0, 0, res.width ,res.height, res.pixels ,0 ,res.width);
 			
 			for(int i = 0; i < res.pixels.length; i++){
-				res.pixels[i] = res.pixels[i] & 0xffffff; //To delete Alpha: 0x'ff'ffffff
+			  res.pixels[i] = res.pixels[i] & 0xffffff; //To delete Alpha: 0x'ff'ffffff
 			}
-			return res;
+			  return res;
 			
-		} catch (IOException e) {
-			
-			e.printStackTrace();
+		} catch (IOException e){
+			  e.printStackTrace();
 		}
 		return null;
-	}	
+	}
+	
 }
