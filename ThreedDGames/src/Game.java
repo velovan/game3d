0d41529eb69fdde99bce41203177a62a147317e8
velovan <velovan@users.noreply.github.com>
2023-01-27 10:02:08 +0200

    Update Game.java

--- ThreedDGames/src/Game.java
+++ ThreedDGames/src/Game.java
@@ -1,11 +1,11 @@
 
-public class Game{
+public class Game {
 
 	public int time;
-	public void Game(){	
+	public void Game() {	
 	}
 	
-	public void update(){
+	public void update() {
            time++;
 	}	
 }
